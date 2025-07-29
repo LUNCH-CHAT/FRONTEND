03f7d5f5d23eb7c2a4fdb8eaaff31465eb9fd190
somin2352 <somin030502@gmail.com>
2025-07-30 01:28:53 +0900

    Feat: stompjs, sockjs 설치 및 useWebsocket 훅 추가
    - 웹소켓 연결, 메시지 송수신, 연결 종료 함수 로직 구현

---
+++ src/hooks/useWebSocket.ts
@@ -0,0 +1,119 @@
+import { Client, type IFrame, type IMessage } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+import { useEffect, useRef, useState } from 'react';
+
+export const webSocketStatus = {
+  CONNECTING: 'CONNECTING',
+  OPEN: 'OPEN',
+  CLOSED: 'CLOSED',
+  RECONNECTING: 'RECONNECTING',
+} as const;
+
+export type WebSocketStatus = (typeof webSocketStatus)[keyof typeof webSocketStatus];
+
+export interface MessageType {
+  senderId: number;
+  content: string;
+  createdAt: Date | null;
+}
+
+export const useWebSocket = (roomId: string) => {
+  const [status, setStatus] = useState<WebSocketStatus>('CONNECTING');
+  // stomp 연결 후 생성한 client 관리
+  const wsClientRef = useRef<Client | null>(null);
+  // 수신 채팅 관리
+  const [message, setMessage] = useState<MessageType>({
+    senderId: 0,
+    content: '',
+    createdAt: null,
+  });
+
+  // 웹소켓 연결
+  const connectWebSocket = () => {
+    if (wsClientRef.current?.connected) {
+      console.log('websocket already connected');
+      return;
+    }
+
+    setStatus('CONNECTING');
+
+    // stomp 클라이언트 객체 생성
+    const client = new Client({
+      webSocketFactory: () => new SockJS(import.meta.env.VITE_WS_URL),
+      reconnectDelay: 5000, // 자동 재연결 시도 간격
+
+      // 웹소켓 연결 성공 시
+      onConnect: (frame: IFrame) => {
+        console.log('websocket connected', frame);
+        setStatus('OPEN');
+
+        // 채팅방 구독 및 메시지 수신
+        client.subscribe(`/sub/rooms/${roomId}`, (message: IMessage) => {
+          const recievedMessage = JSON.parse(message.body);
+          setMessage({
+            senderId: recievedMessage.senderId,
+            content: recievedMessage.content,
+            createdAt: recievedMessage.createdAt,
+          });
+        });
+      },
+
+      // 웹소켓 연결이 끊어졌을 시
+      onWebSocketClose: close => {
+        console.warn('websocket closed, reconnect trying...', close);
+        setStatus('CLOSED');
+      },
+
+      // 웹소켓 오류 발생 시
+      onWebSocketError: error => {
+        console.error('websocket error', error);
+        setStatus('RECONNECTING');
+      },
+
+      // stomp 프로토콜 오류 발생 시
+      onStompError: frame => {
+        console.error('STOMP error: ' + frame.headers['message']);
+        console.error('Additional details: ' + frame.body);
+        setStatus('RECONNECTING');
+      },
+    });
+    wsClientRef.current = client;
+    client.activate(); // Client 활성화
+  };
+
+  // 메시지 전송
+  const sendMessage = (userId: number, message: string) => {
+    const client = wsClientRef.current;
+    if (client && client.connected) {
+      client.publish({
+        destination: `/pub/chat/${roomId}`,
+        body: JSON.stringify({
+          senderId: userId,
+          content: message,
+        }),
+      });
+    }
+  };
+
+  // 웹소켓 연결 해제
+  const disconnectWebSocket = () => {
+    console.log('websocket closed');
+    setStatus('CLOSED');
+
+    if (wsClientRef.current) {
+      wsClientRef.current.deactivate();
+      wsClientRef.current = null;
+    }
+  };
+
+  useEffect(() => {
+    connectWebSocket();
+
+    return () => {
+      disconnectWebSocket();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [roomId]); // roomId가 변경될 때마다 재연결
+
+  return { status, message, sendMessage };
+};
