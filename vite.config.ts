5fb90d11601937a81fe3a8105a9973667fb62d54
jam-jang <jamesjang0622@naver.com>
2025-07-30 20:01:00 +0900

    proxy 설정에서 VITE_API_URL 환경변수 로드 문제 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,28 @@
-// vite.config.ts
-
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
-export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
+export default defineConfig(({ mode }) => {
+  // 👇 load .env file variables
+  const env = loadEnv(mode, process.cwd(), '');
 
- 
-  server: {
-    proxy: {
-      
-      '^/api/.*': {
-        target: 'https://lunchchat.kro.kr',
-        changeOrigin: true,
-        secure: false,
+  return {
+    plugins: [react(), tailwindcss(), svgr()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
       },
     },
-  },
- 
+    server: {
+      proxy: {
+        '^/api/.*': {
+          target: env['VITE_API_URL'], 
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
 });
