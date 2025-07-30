6b82fda390ed4217f69e4af1892c7ddc035de515
jam-jang <jamesjang0622@naver.com>
2025-07-30 20:11:38 +0900

    proxy 설정에서 VITE_API_URL 환경변수 로드 문제 해결2

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/* eslint-env node */
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -5,7 +6,6 @@ import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
 export default defineConfig(({ mode }) => {
-  // 👇 load .env file variables
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
@@ -18,7 +18,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '^/api/.*': {
-          target: env['VITE_API_URL'], 
+          target: env['VITE_API_URL'],
           changeOrigin: true,
           secure: false,
         },
