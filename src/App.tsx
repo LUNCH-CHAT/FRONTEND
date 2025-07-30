0ce864f61130f8e1826c5f5fec42d7cc4b88a8d7
somin2352 <somin030502@gmail.com>
2025-07-30 18:42:46 +0900

    Chore: tanstack query devtools 설치 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 // src/App.tsx
 import './index.css';
 import { createBrowserRouter, RouterProvider, type RouteObject } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import PublicLayout from './layouts/public-layout';
 import OnboardingPage from './pages/login-page/onboarding-page';
@@ -61,6 +63,8 @@ const protectedRoutes: RouteObject[] = [
 
 const router = createBrowserRouter([...publicRoutes, ...protectedRoutes]);
 
+const queryClient = new QueryClient();
+
 function App() {
   // store된 로그인 사용자 ID 가져와서 전달
   // useFCM(memberId);
@@ -79,7 +83,12 @@ function App() {
     return () => unsubscribe();
   }, []);
 
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
+  );
 }
 
 export default App;
